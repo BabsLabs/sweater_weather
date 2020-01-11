26523936f4195e8ac605ccb462cdfa7c8b10c705
Brian Bower <brian@brianbower.com>
2020-01-11 16:34:17 -0700

    Update to use single quotes instead of double quotes to improve runtime

--- spec/services/google_api_service_spec.rb
+++ spec/services/google_api_service_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe GoogleAPIService do
-  it "makes and instance of a GoogleAPIService", :vcr do
+  it 'makes and instance of a GoogleAPIService', :vcr do
 
-    location = "denver, co"
+    location = 'denver, co'
     google_api_service = GoogleAPIService.new(location)
 
     expect(google_api_service).to be_a GoogleAPIService
 
   end
 
-  it "can get the latitude and longitude from a location", :vcr do
+  it 'can get the latitude and longitude from a location', :vcr do
 
-    location = "denver, co"
+    location = 'denver, co'
     google_api_service = GoogleAPIService.new(location)
 
     expect(google_api_service.get_latitude_and_longitude).to eq({:lat=>39.7392358, :lng=>-104.990251})
