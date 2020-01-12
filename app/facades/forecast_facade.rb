07c959bcfbb6bdde1f1d21477a077435772fb3e2
Brian Bower <brian@brianbower.com>
2020-01-12 12:10:46 -0700

    Add eight hour forecast tests and poro to gather the forecast information for the next 8 hours

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,5 +1,5 @@
 class ForecastFacade
-
+  attr_reader :current_forecast, :eight_hour_forecast
 
   def initialize(forecast_info)
     @current_forecast = current_forecast(forecast_info)
@@ -7,7 +7,11 @@ class ForecastFacade
   end
 
   def current_forecast(forecast_info)
-    current_forecast = CurrentWeather.new(forecast_info)
+    CurrentWeather.new(forecast_info)
+  end
+
+  def eight_hour_forecast(forecast_info)
+    EightHourForecast.new(forecast_info)
   end
 
 end
---
+++ spec/poros/eight_hour_forecast_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe EightHourForecast do
+  it 'makes and instance of a EightHourForecast' do
+    weather_json = File.read('spec/fixtures/weather_response.json')
+    parsed_weather = JSON.parse(weather_json, symbolize_names: true)
+
+    forecast = EightHourForecast.new(parsed_weather)
+
+    expect(forecast).to be_a EightHourForecast
+  end
+
+  it 'can find the weather for the next eight hours' do
+    weather_json = File.read('spec/fixtures/weather_response.json')
+    parsed_weather = JSON.parse(weather_json, symbolize_names: true)
+
+    eight_hour_forecast = EightHourForecast.new(parsed_weather)
+
+    expect(eight_hour_forecast.eight_hour_weather).to be_an Array
+    expect(eight_hour_forecast.eight_hour_weather.count).to eq(8)
+    expect(eight_hour_forecast.eight_hour_weather[0]).to be_a Hash
+    expect(eight_hour_forecast.eight_hour_weather[0]).to have_key :time
+    expect(eight_hour_forecast.eight_hour_weather[0]).to_not have_key :cloudCover
+  end
+end
