10fde5c24c4387eb5078b5710459f8f29360c0b3
Brian Bower <brian@brianbower.com>
2020-01-12 13:14:24 -0700

    Dream drive a five day forecast being called in the forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,7 +4,10 @@ class ForecastFacade
   def initialize(forecast_info)
     @current_forecast = current_forecast(forecast_info)
     @eight_hour_forecast = eight_hour_forecast(forecast_info)
+    @five_day_forecast = five_day_forecast(forecast_info)
   end
+
+  private
 
   def current_forecast(forecast_info)
     CurrentWeather.new(forecast_info)
@@ -12,6 +15,10 @@ class ForecastFacade
 
   def eight_hour_forecast(forecast_info)
     EightHourForecast.new(forecast_info)
+  end
+
+  def five_day_forecast(forecast_info)
+    FiveDayForecast.new(forecast_info)
   end
 
 end
