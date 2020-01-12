c6ec600820db0a9ed7767db8d8d0bec3fcaf0207
Brian Bower <brian@brianbower.com>
2020-01-12 13:47:19 -0700

    Update test to check for endpoint - still needs further development

--- spec/requests/api/v1/forecast/finds_weather_for_a_city_spec.rb
+++ spec/requests/api/v1/forecast/finds_weather_for_a_city_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe 'Forecast API Endpoints' do
-  describe 'it finds the weather for a city' do
-    it ' gets the wather for a given location' do
+  describe 'for a given location' do
+    it ' gets the weather forecast info for the front end', :vcr do
+
       search_location = 'denver,co'
 
       get "/api/v1/forecast?location=#{search_location}"
 
       expect(response).to be_successful
-
-      expect(response.body).to eq({'data':'stuff'})
     end
   end
 end
