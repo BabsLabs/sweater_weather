0b9a6ea4325dee5a517dabfe57df396fc6d15c19
Brian Bower <brian@brianbower.com>
2020-01-12 11:22:18 -0700

    Update VCR configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<GOOGLE_GEOCODE_API_KEY>") { ENV['GOOGLE_GEOCODE_API_KEY'] }
+  config.filter_sensitive_data("<DARK_SKY_API_KEY>") { ENV['DARK_SKY_API_KEY'] }
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
