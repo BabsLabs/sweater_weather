05d03a91ddb2d48bbbe060777b08bbdedc4cb77a
Brian Bower <brian@brianbower.com>
2020-01-12 13:49:08 -0700

    Update to make connection method private

--- app/services/google_api_service.rb
+++ app/services/google_api_service.rb
@@ -1,27 +1,27 @@
 class GoogleAPIService
+  attr_reader :latitude_and_longitude
 
   def initialize(location)
-    @location = location
-  end
-
-  def get_latitude_and_longitude
-    response = conn.get('/maps/api/geocode/json') do |req|
-      req.params['address'] = @location
-    end
-
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
-
-    # returns the hash of latitude and longitude
-    parsed_response[:results].first[:geometry][:location]
+    @latitude_and_longitude = get_latitude_and_longitude(location)
   end
 
   private
 
-  def conn
-    Faraday.new(url: 'https://maps.googleapis.com') do |f|
-      f.params['key'] = ENV['GOOGLE_GEOCODE_API_KEY']
-      f.adapter Faraday.default_adapter
-    end
-  end
+    def get_latitude_and_longitude(location)
+      response = conn.get('/maps/api/geocode/json') do |req|
+        req.params['address'] = location
+      end
 
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      # returns the hash of latitude and longitude
+      parsed_response[:results].first[:geometry][:location]
+    end
+
+    def conn
+      Faraday.new(url: 'https://maps.googleapis.com') do |f|
+        f.params['key'] = ENV['GOOGLE_GEOCODE_API_KEY']
+        f.adapter Faraday.default_adapter
+      end
+    end
 end
