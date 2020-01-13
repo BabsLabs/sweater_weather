a5ef44e5dc79ba78cc4a7de6c46a20d8659e6ddc
Brian Bower <brian@brianbower.com>
2020-01-13 10:42:37 -0700

    Add comments about the long reassignment and add AntipodeForecastFacade

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -9,11 +9,15 @@ class Api::V1::AntipodeController < ApplicationController
 
     antipode_city_info = GoogleAPIService.new(nil , antipode_info_for_google_reverse_lookup)
 
+    # reassign the key returned from the antipode service to be reused in the darksky service
+    # break out into its own method or refactor in the final facade if there is time
     antipode_info_for_google_reverse_lookup[:lng] = antipode_info_for_google_reverse_lookup[:long]
 
     forecast_results = DarkSkyAPIService.new(antipode_info_for_google_reverse_lookup)
 
-    antipode_facade = AntipodeForecastFacade.new(forecast_results)
+    antipode_forecast_facade = AntipodeForecastFacade.new(forecast_results)
+
+binding.pry
 
   end
 
