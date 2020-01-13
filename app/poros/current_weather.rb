5ad57901cdb9079105cc0b821c96b3674ad7dc5f
Brian Bower <brian@brianbower.com>
2020-01-13 15:31:55 -0700

    Add current time and today summary to the current weather

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -1,5 +1,6 @@
 class CurrentWeather
-  attr_reader :current_temperature,
+  attr_reader :current_time,
+              :current_temperature,
               :current_weather_icon,
               :current_weather_summary,
               :todays_high,
@@ -7,9 +8,12 @@ class CurrentWeather
               :feels_like,
               :humidity,
               :visibility,
-              :uv_index
+              :uv_index,
+              :today_summary
+              # :tonight_summary
 
   def initialize(forecast_info)
+    @current_time = forecast_info[:currently][:time]
     @current_temperature = forecast_info[:currently][:temperature].to_i
     @current_weather_icon = forecast_info[:currently][:icon]
     @current_weather_summary = forecast_info[:currently][:summary]
@@ -19,6 +23,14 @@ class CurrentWeather
     @humidity = forecast_info[:currently][:humidity].to_i
     @visibility = forecast_info[:currently][:visibility]
     @uv_index = forecast_info[:currently][:uvIndex].to_i
+    @today_summary = forecast_info[:daily][:data][0][:summary]
+    # @tonight_summary = get_tonights_summary(forecast_info)
   end
+  #
+  # private
+  #
+  #   def get_tonights_summary(forecast_info)
+  #     forecast_info[:daily][:data][0][:time]
+  #   end
 
 end
