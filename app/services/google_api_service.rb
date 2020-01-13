e698f6179975bb0434429ddacca11aec6fa2b8f1
Brian Bower <brian@brianbower.com>
2020-01-13 10:39:24 -0700

    Add second arguments to google service so it can do a reverse lookup

--- app/services/google_api_service.rb
+++ app/services/google_api_service.rb
@@ -1,8 +1,9 @@
 class GoogleAPIService
-  attr_reader :latitude_and_longitude
+  attr_reader :latitude_and_longitude, :city_name_from_lat_and_long
 
-  def initialize(location)
-    @latitude_and_longitude = get_latitude_and_longitude(location)
+  def initialize(location_name = nil, lat_and_long = nil )
+    @latitude_and_longitude = get_latitude_and_longitude(location_name) if location_name != nil
+    @city_name_from_lat_and_long = get_city_name(lat_and_long) if lat_and_long != nil
   end
 
   private
@@ -16,6 +17,17 @@ class GoogleAPIService
 
       # returns the hash of latitude and longitude
       parsed_response[:results].first[:geometry][:location]
+    end
+
+
+    def get_city_name(lat_and_long)
+      response = conn.get('/maps/api/geocode/json') do |req|
+        req.params['latlng'] = "#{lat_and_long[:lat]}, #{lat_and_long[:long]}"
+      end
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      parsed_response[:results][0][:address_components][1][:long_name]
     end
 
     def conn
