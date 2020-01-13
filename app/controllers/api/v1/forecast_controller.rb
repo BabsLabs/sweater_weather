6009465b20f040a0038985d8875608b476f989e3
Brian Bower <brian@brianbower.com>
2020-01-12 20:49:32 -0700

    Add weather facade serailizer to be json 1.0 compliant

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     weather_facade = WeatherFacade.new(params[:location])
-    # serialized_weather = WeatherFacadeSerializer.new(weather_facade)
-
-    render json: weather_facade
+    serialized_weather = WeatherFacadeSerializer.new(weather_facade)
+    render json: serialized_weather
   end
 
 end
