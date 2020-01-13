b74dbcfe6d831c33083d66a532cd837fbcfb1fc4
Brian Bower <brian@brianbower.com>
2020-01-13 09:29:47 -0700

    Add test files for antip[ode endpoint

---
+++ spec/requests/api/v1/antipode/gets_the_attributes_for_the_antipode_city_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Antipode API Endpoint' do
+  describe 'for a given location' do
+    it ' gets the antipode city name and weather forecast', :vcr do
+
+      search_location = 'hong kong'
+
+      get "/api/v1/antipode?location=#{search_location}"
+
+      expect(response).to be_successful
+
+      expected_response = File.read('spec/fixtures/antipode_response.json')
+
+      expect(response.body).to eq(expected_response)
+    end
+  end
+end
