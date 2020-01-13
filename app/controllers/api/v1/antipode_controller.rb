07125c67848336d89cdbd8464dec3f76b8dc62b7
Brian Bower <brian@brianbower.com>
2020-01-13 10:39:01 -0700

    Add services and adjust dataflow

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::AntipodeController < ApplicationController
 
   def show
-    location = GoogleAPIService.new(params[:location])
+    location = GoogleAPIService.new(params[:location], nil)
 
     antipode = AntipodeService.new(location.latitude_and_longitude)
 
-    
+    antipode_info_for_google_reverse_lookup = antipode.antipode_info[:data][:attributes]
+
+    antipode_city_info = GoogleAPIService.new(nil , antipode_info_for_google_reverse_lookup)
+
+    antipode_info_for_google_reverse_lookup[:lng] = antipode_info_for_google_reverse_lookup[:long]
+
+    forecast_results = DarkSkyAPIService.new(antipode_info_for_google_reverse_lookup)
+
+    antipode_facade = AntipodeForecastFacade.new(forecast_results)
 
   end
 
