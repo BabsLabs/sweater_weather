710b6852e6ab5b491aad930a4851d2aa7f1e7a29
Brian Bower <brian@brianbower.com>
2020-01-13 09:48:02 -0700

    Add antipode service to controller to get the antipode lat and long for the queried location

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::AntipodeController < ApplicationController
 
   def show
+    location = GoogleAPIService.new(params[:location])
+
+    antipode = AntipodeService.new(location.latitude_and_longitude)
+
     
+
   end
 
 end
