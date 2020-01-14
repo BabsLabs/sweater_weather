1326ba006ff19d55ccc13235a2d6e7bb3cffdf4a
Brian Bower <brian@brianbower.com>
2020-01-14 15:53:11 -0700

    Add controller actions to find and render the json for the frontend app

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -7,21 +7,28 @@ class Api::V1::RoadTripController < ApplicationController
       google_service = GoogleAPIService.new
       directions = google_service.get_driving_directions(params[:origin], params[:destination])
       drive_time = directions[:routes][0][:legs][0][:duration][:text]
+
+      drive_time_in_seconds = directions[:routes][0][:legs][0][:duration][:value]
+      arrival_time = (Time.now + drive_time_in_seconds).to_i
+
       lat_and_long_of_destination = directions[:routes][0][:legs][0][:end_location]
       dark_sky_service = DarkSkyAPIService.new(lat_and_long_of_destination)
+
       forecast_results = dark_sky_service.get_forecast
 
       hourly_arrival_forecast = forecast_results[:hourly][:data]
 
-      array = []
-      hourly_arrival_forecast.each do |hourly_info|
-        binding.pry
-        #remove the letters and spaces from the drive time and make it an int
-        arrival_time = hourly_info[:time] + drive_time.delete("^0-9").to_i
+      forecast_on_arrival = hourly_arrival_forecast.select do |h|
+        h[:time] >= arrival_time
+      end.first
 
-      end
+      temp_and_summary_on_arrival = forecast_on_arrival.slice(:summary, :temperature)
 
+      trip = Trip.new(temp_and_summary_on_arrival, drive_time, params[:origin], params[:destination])
 
+      serialized_trip = TripSerializer.new(trip)
+
+      render json: serialized_trip
     end
   end
 
