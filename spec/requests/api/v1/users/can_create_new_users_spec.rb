b65c322645b224c685b2e13ac7dc251a5684425f
Brian Bower <brian@brianbower.com>
2020-01-13 23:38:33 -0700

    Add params to be passed in

--- spec/requests/api/v1/users/can_create_new_users_spec.rb
+++ spec/requests/api/v1/users/can_create_new_users_spec.rb
@@ -4,7 +4,7 @@ describe 'Users API Endpoints' do
   describe 'for a given user' do
     it 'returns the api key for a newly created user', :vcr do
 
-      post "/api/v1/users"
+      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password_confirmation'}
 
       expect(response).to be_successful
     end
