d4b128e4c72ca3becd226d75e8ad4386c0b7b7d5
Brian Bower <brian@brianbower.com>
2020-01-14 10:08:48 -0700

    Add test for sad paths

--- spec/requests/api/v1/users/can_create_new_users_spec.rb
+++ spec/requests/api/v1/users/can_create_new_users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Users API Endpoints' do
   describe 'for a given user' do
     it 'returns the api key for a newly created user', :vcr do
-      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password_confirmation'}
+      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password'}
 
       expect(response).to be_successful
       expect(User.last.email).to eq('email@email.com')
@@ -14,14 +14,32 @@ describe 'Users API Endpoints' do
       expect(parsed_response[:data][:attributes][:api_key]).to eq(User.last.api_key)
     end
 
-    it 'returns shows an error if something was not provided', :vcr do
-      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password_confirmation'}
-      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password_confirmation'}
+    it 'returns shows an error if email was not provided', :vcr do
+      post "/api/v1/users", params: {email: '', password: 'password', password_confirmation: 'password'}
 
-      expect(response).to be_successful
+      expect(response).to_not be_successful
 
       parsed_response = JSON.parse(response.body, symbolize_names: true)
-      expect(parsed_response).to eq({:status=>"error", :code=>400, :message=>"Email has already been taken"})
+      expect(parsed_response).to eq({:message=>"Email can't be blank"})
+    end
+
+    it 'returns shows an error if password was not provided', :vcr do
+      post "/api/v1/users", params: {email: 'email@email.com', password: '', password_confirmation: 'password'}
+
+      expect(response).to_not be_successful
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response).to eq({:message=>"Password can't be blank"})
+    end
+
+    it 'returns shows an error if a user tried to use the same email', :vcr do
+      User.create(email: 'email@email.com', password: 'password', password_confirmation: 'password')
+      post "/api/v1/users", params: {email: 'email@email.com', password: 'password', password_confirmation: 'password'}
+
+      expect(response).to_not be_successful
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response).to eq({:message=>"Email has already been taken"})
     end
   end
 end
