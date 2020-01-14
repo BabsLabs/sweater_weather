42815add6a7e1c586d5a03eb4e4dcdc513b63c96
Brian Bower <brian@brianbower.com>
2020-01-14 00:01:07 -0700

    Change api token to come back as `api_key` instead of `token`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_secure_token
+  has_secure_token :api_key
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
