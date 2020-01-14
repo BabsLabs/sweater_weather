82746f5aa5efe06adf0fd4b9f5061650dc6cb3aa
Brian Bower <brian@brianbower.com>
2020-01-14 12:03:43 -0700

    Add tests for result keys

--- spec/requests/api/v1/forecast/gets_the_weather_forecast_spec.rb
+++ spec/requests/api/v1/forecast/gets_the_weather_forecast_spec.rb
@@ -9,6 +9,25 @@ describe 'Forecast Endpoint' do
       get "/api/v1/forecast?location=#{search_location}"
 
       expect(response).to be_successful
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :data
+      expect(parsed_response[:data]).to have_key :id
+      expect(parsed_response[:data]).to have_key :attributes
+      expect(parsed_response[:data][:attributes]).to have_key :id
+      expect(parsed_response[:data][:attributes]).to have_key :weather_information
+      expect(parsed_response[:data][:attributes][:weather_information]).to have_key :current_forecast
+      expect(parsed_response[:data][:attributes][:weather_information][:current_forecast]).to have_key :current_time
+      expect(parsed_response[:data][:attributes][:weather_information][:eight_hour_forecast]).to have_key :eight_hour_weather
+      expect(parsed_response[:data][:attributes][:weather_information][:eight_hour_forecast][:eight_hour_weather][0]).to have_key :time
+      expect(parsed_response[:data][:attributes][:weather_information][:eight_hour_forecast][:eight_hour_weather][0]).to have_key :temperature
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast]).to have_key :five_day_weather
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast][:five_day_weather][0]).to have_key :time
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast][:five_day_weather][0]).to have_key :icon
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast][:five_day_weather][0]).to have_key :temperatureHigh
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast][:five_day_weather][0]).to have_key :temperatureLow
+      expect(parsed_response[:data][:attributes][:weather_information][:five_day_forecast][:five_day_weather][0]).to have_key :precipProbability
     end
   end
 end
