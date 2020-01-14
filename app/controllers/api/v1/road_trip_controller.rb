d0d0504c8bff3bfec7b64f0a173e8828efeb956b
Brian Bower <brian@brianbower.com>
2020-01-14 13:04:36 -0700

    Add iteration through hours

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -11,8 +11,16 @@ class Api::V1::RoadTripController < ApplicationController
       dark_sky_service = DarkSkyAPIService.new(lat_and_long_of_destination)
       forecast_results = dark_sky_service.get_forecast
 
-      # binding.pry
-      arrival_forecast = forecast_results
+      hourly_arrival_forecast = forecast_results[:hourly][:data]
+
+      array = []
+      hourly_arrival_forecast.each do |hourly_info|
+        binding.pry
+        #remove the letters and spaces from the drive time and make it an int
+        arrival_time = hourly_info[:time] + drive_time.delete("^0-9").to_i
+
+      end
+
 
     end
   end
