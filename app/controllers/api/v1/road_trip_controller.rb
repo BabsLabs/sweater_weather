bb07020716d24ba18965b0af4316bf9efd44cd5e
Brian Bower <brian@brianbower.com>
2020-01-14 12:03:33 -0700

    Remove pry

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::RoadTripController < ApplicationController
       dark_sky_service = DarkSkyAPIService.new(lat_and_long_of_destination)
       forecast_results = dark_sky_service.get_forecast
 
-      binding.pry
+      # binding.pry
       arrival_forecast = forecast_results
 
     end
