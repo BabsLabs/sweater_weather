048b972f2c3315dc07b0f2e553d10630cbb67db1
Brian Bower <brian@brianbower.com>
2020-01-14 00:01:17 -0700

    Add user serializer to be rendered

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
+    serialized_user = UserSerializer.new(user)
+    render json: serialized_user
   end
 
   private
