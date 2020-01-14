7492324e034371c811e8b6a40f474fd10f74c392
Brian Bower <brian@brianbower.com>
2020-01-14 10:09:49 -0700

    Add password confirm to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_secure_token :api_key
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, require: true
+  validates_presence_of :password_confirmation, require: true, length: { minimum: 1 }
 end
