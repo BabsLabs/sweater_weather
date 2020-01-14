7ae3f7fe5a949f50955b4c739dc60b621e6ae293
Brian Bower <brian@brianbower.com>
2020-01-14 07:39:46 -0700

    Add error message handling for incorrectly created user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    serialized_user = UserSerializer.new(user)
-    render json: serialized_user
+    if user.save
+      serialized_user = UserSerializer.new(user)
+      render json: serialized_user
+    else
+      render json: {status: "error", code: 400, message: user.errors.full_messages.to_sentence}
+    end
   end
 
   private
