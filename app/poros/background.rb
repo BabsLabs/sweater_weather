b312484bc7e63571e50c43bb83e19cf35feb39b3
Brian Bower <brian@brianbower.com>
2020-01-15 12:57:50 -0700

    Add default image if bad location is searched for

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -4,7 +4,15 @@ class Background
   attr_reader :id, :url
 
   def initialize(background_data)
-    @url = background_data[:results][0][:urls][:raw]
+    @url = return_bg_or_error(background_data)
     @id = SecureRandom.hex(10)
+  end
+
+  def return_bg_or_error(background_data)
+    if background_data[:results] != []
+      background_data[:results][0][:urls][:raw]
+    else
+      return "https://images.unsplash.com/photo-1557862471-150b86c49ca1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=80"
+    end
   end
 end
