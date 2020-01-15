ad48d83d28825ef42dca908dbb95d9e0cf6995d8
Brian Bower <brian@brianbower.com>
2020-01-15 12:21:53 -0700

    Refactor Unsplash service to use facades instead of direct service calls

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::BackgroundsController < ApplicationController
 
   def show
-    background_url = UnsplashAPIService.new(params[:location])
-    serialized_url = UnsplashApiServiceSerializer.new(background_url)
-    render json: serialized_url
+    background = BackgroundFacade.find_background(params[:location])
+    serialized_background = BackgroundSerializer.new(background)
+    render json: serialized_background
   end
 
 end
