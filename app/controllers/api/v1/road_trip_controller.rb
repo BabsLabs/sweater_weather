6c921baa871ba3da8e3e09fa7190675aeed2bfae
Brian Bower <brian@brianbower.com>
2020-01-15 07:23:26 -0700

    refactor serializer into own method

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,21 +3,17 @@ class Api::V1::RoadTripController < ApplicationController
   def show
     if !params[:api_key] || !User.find_by(api_key: params[:api_key])
       render json: {message: 'Did you provide a correct api key?'}, status: 401
-    elsif
+    elsif User.find_by(api_key: params[:api_key])
       all_directions = get_all_direction_info(params[:origin], params[:destination])
-      drive_time_string = get_drive_time_string(all_directions)
-      drive_time_in_seconds = get_drive_time_seconds(all_directions)
-      arrival_time = get_arrival_time_in_seconds(all_directions, drive_time_in_seconds)
+      drive_time_info = get_travel_time_info(all_directions)
+      arrival_time = get_arrival_time_in_seconds(all_directions, drive_time_info[1])
       lat_and_long_of_destination = get_lat_and_long_of_destination(all_directions)
-      forecast_results = get_forecast_results(lat_and_long_of_destination)
-      hourly_arrival_forecast = get_hourly_forecast_data(forecast_results)
+      hourly_arrival_forecast = get_hourly_arrival_forecast(lat_and_long_of_destination)
       temp_and_summary_on_arrival = get_current_weather_on_arrival_info(hourly_arrival_forecast, arrival_time)
 
-
-      trip = Trip.new(temp_and_summary_on_arrival, drive_time_string, params[:origin], params[:destination])
-      serialized_trip = TripSerializer.new(trip)
-
-      render json: serialized_trip
+      render json: serialized_trip(temp_and_summary_on_arrival, drive_time_info[0], params[:origin], params[:destination])
+    elsif
+      render json: {message: 'There was an error. The most likely reason is your destination is further than 48 hours away. Please pick a stopping destination closer by.'}, status: 200
     end
   end
 
@@ -25,6 +21,13 @@ class Api::V1::RoadTripController < ApplicationController
 
     def get_all_direction_info(origin, destination)
       GoogleAPIService.get_driving_directions(origin, destination)
+    end
+
+    def get_travel_time_info(all_directions)
+      travel_time_array = []
+      travel_time_array[0] = get_drive_time_string(all_directions) if get_drive_time_string(all_directions) != 0
+      travel_time_array[1] = get_drive_time_seconds(all_directions)
+      travel_time_array
     end
 
     def get_drive_time_string(all_directions)
@@ -47,7 +50,8 @@ class Api::V1::RoadTripController < ApplicationController
       DarkSkyAPIService.get_forecast(lat_and_long_of_destination)
     end
 
-    def get_hourly_forecast_data(forecast_results)
+    def get_hourly_arrival_forecast(lat_and_long_of_destination)
+      forecast_results = get_forecast_results(lat_and_long_of_destination)
       forecast_results[:hourly][:data]
     end
 
@@ -56,6 +60,11 @@ class Api::V1::RoadTripController < ApplicationController
         h[:time] >= arrival_time
       end.first
       hourly_arrival_forecast[0].slice(:summary, :temperature)
+    end
+
+    def serialized_trip(temp_and_summary_on_arrival, drive_time_string, origin, destination)
+      trip = Trip.new(temp_and_summary_on_arrival, drive_time_string, origin, destination)
+      TripSerializer.new(trip)
     end
 
 end
