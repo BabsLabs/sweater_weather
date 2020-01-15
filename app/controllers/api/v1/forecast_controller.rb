14d716683099796a18c669a12d0d19b59d998c7f
Brian Bower <brian@brianbower.com>
2020-01-14 18:42:11 -0700

    Refactor to render the serialized Forecast instead of the serialized WeatherFacade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    weather_facade = WeatherFacade.new(params[:location])
-    serialized_weather = WeatherFacadeSerializer.new(weather_facade)
-    render json: serialized_weather
+    forecast = WeatherFacade.get_weather_information(params[:location])
+    serialized_forecast = ForecastSerializer.new(forecast)
+    render json: serialized_forecast
   end
 
 end
