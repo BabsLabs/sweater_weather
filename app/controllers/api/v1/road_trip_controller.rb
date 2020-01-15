d95210486f21bc552fba9286e7c4c1f617c59c84
Brian Bower <brian@brianbower.com>
2020-01-14 19:15:32 -0700

    Refactor service to call Class methods when possible

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,17 +4,14 @@ class Api::V1::RoadTripController < ApplicationController
     if !params[:api_key] || !(user = User.find_by(api_key: params[:api_key]))
       render json: {message: 'Did you provide a correct api key?'}, status: 401
     elsif
-      google_service = GoogleAPIService.new
-      directions = google_service.get_driving_directions(params[:origin], params[:destination])
+      directions = GoogleAPIService.get_driving_directions(params[:origin], params[:destination])
       drive_time = directions[:routes][0][:legs][0][:duration][:text]
 
       drive_time_in_seconds = directions[:routes][0][:legs][0][:duration][:value]
       arrival_time = (Time.now + drive_time_in_seconds).to_i
 
       lat_and_long_of_destination = directions[:routes][0][:legs][0][:end_location]
-      dark_sky_service = DarkSkyAPIService.new(lat_and_long_of_destination)
-
-      forecast_results = dark_sky_service.get_forecast
+      forecast_results = DarkSkyAPIService.get_forecast(lat_and_long_of_destination)
 
       hourly_arrival_forecast = forecast_results[:hourly][:data]
 
